9d8ef35bec3afaa26fd464436043fc5084a97a63
Mikhail Korobov <kmike84@gmail.com>
2019-08-30 16:17:16 +0500

    example of using page objects directly (doesn't work)

---
+++ example/example/spiders/books_02_3.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+This example doesn't work!
+
+Scrapy spider which uses Page Objects to make extraction code more reusable.
+BookPage is now independent of Scrapy.
+
+Page object is used instead of callback below. It doesn't work now,
+but it can be implemented, with Scrapy support.
+"""
+import scrapy
+from scrapy_po import WebPage
+
+
+class BookPage(WebPage):
+    def to_item(self):
+        return {
+            'url': self.url,
+            'name': self.css("title::text").get(),
+        }
+
+
+class BooksSpider(scrapy.Spider):
+    name = 'books_02_3'
+    start_urls = ['http://books.toscrape.com/']
+
+    def parse(self, response):
+        for url in response.css('.image_container a::attr(href)').getall():
+            yield response.follow(url, BookPage)
