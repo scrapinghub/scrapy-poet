362085d462dd53c1687e9ce767cd8e2cda7898d5
Mikhail Korobov <kmike84@gmail.com>
2020-04-28 01:38:39 +0500

    enable Python 3.8 tests, declare Python 3.8 support

--- setup.py
+++ setup.py
@@ -22,5 +22,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
