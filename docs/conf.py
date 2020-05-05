e1d86cd4aff4ccb6bf8a6afe394b54d4bfc2329d
Victor Torres <vpaivatorres@gmail.com>
2020-05-05 17:45:46 -0300

    install sphinx rtd theme
    it's the same theme used by scrapy's docs

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,13 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom themes here, relative to this directory.
+# Add path to the RTD explicitly to robustify builds (otherwise might
+# fail in a clean Debian build env)
+import sphinx_rtd_theme
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
