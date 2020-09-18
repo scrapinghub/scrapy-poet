0f95a4e1e1a1522fd763204b38f92c30f0ae065e
Victor Torres <vpaivatorres@gmail.com>
2020-09-18 18:45:08 +0100

    Remove test case
    This test case doesn't make sense here because we're not even registering the providers being asserted! We're testing something else which is instance method callbacks.

--- tests/test_callback_for.py
+++ tests/test_callback_for.py
@@ -5,7 +5,6 @@ from scrapy.utils.reqser import request_to_dict
 from web_poet.pages import ItemPage, ItemWebPage
 from scrapy_poet.utils import (
     callback_for,
-    is_response_going_to_be_used,
     DummyResponse,
 )
 
@@ -74,14 +73,12 @@ def test_instance_method_callback():
     assert isinstance(request_dict, dict)
     assert request_dict['url'] == 'http://example.com/'
     assert request_dict['callback'] == 'parse_item'
-    assert is_response_going_to_be_used(request, spider) is False
 
     request = scrapy.Request('http://example.com/', callback=spider.parse_web)
     request_dict = request_to_dict(request, spider)
     assert isinstance(request_dict, dict)
     assert request_dict['url'] == 'http://example.com/'
     assert request_dict['callback'] == 'parse_web'
-    assert is_response_going_to_be_used(request, spider) is True
 
 
 def test_inline_callback():
