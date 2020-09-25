802da45dfbdaeb24f5eecbb6ea9dda0e48f58101
Victor Torres <vpaivatorres@gmail.com>
2020-09-25 15:36:28 +0100

    Remove unused code

--- scrapy_poet/middleware.py
+++ scrapy_poet/middleware.py
@@ -10,11 +10,7 @@ from scrapy.statscollectors import StatsCollector
 from twisted.internet.defer import inlineCallbacks, returnValue
 
 from scrapy_poet import utils
-from scrapy_poet.page_input_providers import PageObjectInputProvider
 from scrapy_poet.utils import _SCRAPY_PROVIDED_CLASSES, load_provider_classes
-
-
-Provider = PageObjectInputProvider
 
 
 class ProviderManager:
