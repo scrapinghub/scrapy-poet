58b4ef1829c4a0baeede5401a2b3ce38cb3c06e6
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2022-01-28 17:09:48 +0800

    Bump version: 0.2.1 → 0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = u'Scrapinghub'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.2.1'
+release = u'0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
