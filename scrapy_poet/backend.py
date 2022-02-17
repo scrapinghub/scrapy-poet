c1bec9b2f6259fa8c7f1c042b9a9254bbff193bc
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2022-02-17 18:08:50 +0800

    use web-poet's new Request container

--- scrapy_poet/backend.py
+++ scrapy_poet/backend.py
@@ -6,7 +6,7 @@ import attr
 import scrapy
 import scrapy_poet
 from web_poet.page_inputs import ResponseData
-from web_poet.requests import GenericRequest, RequestBackendError
+from web_poet.requests import Request, RequestBackendError
 
 
 logger = logging.getLogger(__name__)
@@ -22,10 +22,10 @@ def enable_backend():
 scrapy_downloader_var: ContextVar = ContextVar("downloader")
 
 
-async def scrapy_poet_backend(request: Union[GenericRequest, scrapy.Request]):
+async def scrapy_poet_backend(request: Union[Request, scrapy.Request]):
     """To use this, frameworks must run: ``scrapy_poet.enable_backend()``.
 
-    The request could either be ``web_poet.GenericRequest`` or even a
+    The request could either be ``web_poet.Request`` or even a
     ``scrapy.Request`` to give developers more fine grain control.
     """
 
@@ -34,7 +34,7 @@ async def scrapy_poet_backend(request: Union[GenericRequest, scrapy.Request]):
             request = scrapy.Request(**attr.asdict(request))
         except TypeError:
             raise RequestBackendError(
-                f"The given GenericRequest isn't compatible with `scrapy.Request`. "
+                f"The given Request isn't compatible with `scrapy.Request`. "
                 f"Ensure that it doesn't contain extra fields: {request}"
             )
 
