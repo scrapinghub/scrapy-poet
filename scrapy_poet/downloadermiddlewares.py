af6905a01f6ba6fe008c35d37fcc5889fdc39443
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2023-01-13 19:42:13 +0800

    simplify conditional on checking cb_kwargs

--- scrapy_poet/downloadermiddlewares.py
+++ scrapy_poet/downloadermiddlewares.py
@@ -150,7 +150,7 @@ class InjectionMiddleware:
         for arg, value in final_kwargs.items():
             # If scrapy-poet can't provided the dependency, allow the user to
             # give it.
-            if value is None and request.cb_kwargs.get(arg) is not None:
+            if value is None and arg in request.cb_kwargs:
                 continue
             request.cb_kwargs[arg] = value
             # TODO: check if all arguments are fulfilled somehow?
