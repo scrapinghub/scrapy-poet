f828cdc724b2c40976084b46a923178277b646f3
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2023-01-16 21:38:55 +0800

    bump date 2022 → 2023

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath("../"))
 # -- Project information -----------------------------------------------------
 
 project = "scrapy-poet"
-copyright = "2022, Zyte"
+copyright = "2023, Zyte"
 author = "Zyte"
 
 # The short X.Y version
