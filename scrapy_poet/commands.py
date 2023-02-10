bf1a40b93e283e00cea4b1eff10422be9cdba1e1
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2023-02-10 15:46:46 +0800

    use EPollReactor for SaveFixtureCommand

--- scrapy_poet/commands.py
+++ scrapy_poet/commands.py
@@ -84,6 +84,7 @@ class SaveFixtureCommand(ScrapyCommand):
         spider_cls = spider_for(cls)
         self.settings["ITEM_PIPELINES"][SavingPipeline] = 100
         self.settings["DOWNLOADER_MIDDLEWARES"][SavingInjectionMiddleware] = 543
+        self.settings["TWISTED_REACTOR"] = "twisted.internet.epollreactor.EPollReactor"
 
         frozen_time = datetime.datetime.now(datetime.timezone.utc).replace(
             microsecond=0
