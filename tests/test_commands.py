34e7232b63073042c5b53bef24d5108a106641c6
Andrey Rakhmatullin <wrar@wrar.name>
2023-04-05 17:03:58 +0400

    Add a test for handling Retry in savefixture.

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -7,11 +7,16 @@ from pathlib import Path
 
 from web_poet.testing import Fixture
 
+from scrapy_poet.utils.mockserver import MockServer
+from scrapy_poet.utils.testing import EchoResource
+
 
 def call_scrapy_command(cwd: str, *args: str) -> None:
     with tempfile.TemporaryFile() as out:
         args = (sys.executable, "-m", "scrapy.cmdline") + args
         subprocess.call(args, stdout=out, stderr=out, cwd=cwd)
+        out.seek(0)
+        pass
 
 
 def test_savefixture(tmp_path) -> None:
@@ -96,3 +101,61 @@ class HeadersPage(WebPage):
     assert fixture.is_valid()
     item = json.loads(fixture.output_path.read_bytes())
     assert item == {"ua": "scrapy/savefixture"}
+
+
+def test_savefixture_exceptions_retry(tmp_path) -> None:
+    project_name = "foo"
+    cwd = Path(tmp_path)
+    call_scrapy_command(str(cwd), "startproject", project_name)
+    cwd /= project_name
+    type_name = "foo.po.SamplePage"
+    (cwd / project_name / "po.py").write_text(
+        """
+from collections import deque
+
+from web_poet.exceptions import Retry
+from web_poet.pages import WebPage
+
+
+retries = deque([True, False])
+
+
+class SamplePage(WebPage):
+    def to_item(self):
+        if retries.popleft():
+            raise Retry
+        return {"foo": "bar"}
+"""
+    )
+    (cwd / project_name / "spiders/retry.py").write_text(
+        """
+import scrapy_poet
+from scrapy import Request, Spider
+
+from foo.po import SamplePage
+
+
+class TestSpider(Spider):
+    name = "test_spider"
+
+    custom_settings = {
+        "SPIDER_MIDDLEWARES": {
+            "scrapy_poet.RetryMiddleware": 275,
+        },
+    }
+
+    def parse(self, response, page: SamplePage):
+        items.append(page.to_item())
+"""
+    )
+    with MockServer(EchoResource) as server:
+        call_scrapy_command(
+            str(cwd), "savefixture", type_name, server.root_url, "test_spider"
+        )
+
+    fixtures_dir = cwd / "fixtures"
+    fixture_dir = fixtures_dir / type_name / "test-1"
+    fixture = Fixture(fixture_dir)
+    assert fixture.is_valid()
+    assert (fixture.input_path / "HttpResponse-body.html").exists()
+    assert json.loads(fixture.output_path.read_bytes()) == {"foo": "bar"}
