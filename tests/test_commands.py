851006c806009edc21a69051530afab664d7932d
Andrey Rakhmatullin <wrar@wrar.name>
2023-04-19 19:17:14 +0400

    Support expected exceptions in savefixture.

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -5,7 +5,6 @@ import sys
 import tempfile
 from pathlib import Path
 
-import pytest
 from web_poet.testing import Fixture
 
 from scrapy_poet.utils.mockserver import MockServer
@@ -16,8 +15,6 @@ def call_scrapy_command(cwd: str, *args: str) -> None:
     with tempfile.TemporaryFile() as out:
         args = (sys.executable, "-m", "scrapy.cmdline") + args
         subprocess.call(args, stdout=out, stderr=out, cwd=cwd)
-        out.seek(0)
-        pass
 
 
 def test_savefixture(tmp_path) -> None:
@@ -104,66 +101,7 @@ class HeadersPage(WebPage):
     assert item == {"ua": "scrapy/savefixture"}
 
 
-def test_savefixture_exceptions_retry(tmp_path) -> None:
-    project_name = "foo"
-    cwd = Path(tmp_path)
-    call_scrapy_command(str(cwd), "startproject", project_name)
-    cwd /= project_name
-    type_name = "foo.po.SamplePage"
-    (cwd / project_name / "po.py").write_text(
-        """
-from collections import deque
-
-from web_poet.exceptions import Retry
-from web_poet.pages import WebPage
-
-
-retries = deque([True, False])
-
-
-class SamplePage(WebPage):
-    def to_item(self):
-        if retries.popleft():
-            raise Retry
-        return {"foo": "bar"}
-"""
-    )
-    (cwd / project_name / "spiders/retry.py").write_text(
-        """
-import scrapy_poet
-from scrapy import Request, Spider
-
-from foo.po import SamplePage
-
-
-class TestSpider(Spider):
-    name = "test_spider"
-
-    custom_settings = {
-        "SPIDER_MIDDLEWARES": {
-            "scrapy_poet.RetryMiddleware": 275,
-        },
-    }
-
-    def parse(self, response, page: SamplePage):
-        pass
-"""
-    )
-    with MockServer(EchoResource) as server:
-        call_scrapy_command(
-            str(cwd), "savefixture", type_name, server.root_url, "test_spider"
-        )
-
-    fixtures_dir = cwd / "fixtures"
-    fixture_dir = fixtures_dir / type_name / "test-1"
-    fixture = Fixture(fixture_dir)
-    assert fixture.is_valid()
-    assert (fixture.input_path / "HttpResponse-body.html").exists()
-    assert json.loads(fixture.output_path.read_bytes()) == {"foo": "bar"}
-
-
-@pytest.mark.xfail(reason="The generated test doesn't handle UseFallback.")
-def test_savefixture_exceptions_usefallback(tmp_path) -> None:
+def test_savefixture_expected_exception(tmp_path) -> None:
     project_name = "foo"
     cwd = Path(tmp_path)
     call_scrapy_command(str(cwd), "startproject", project_name)
@@ -180,11 +118,9 @@ class SamplePage(WebPage):
         raise UseFallback
 """
     )
-    (cwd / project_name / "spiders/retry.py").write_text(
+    (cwd / project_name / "spiders/test.py").write_text(
         """
-import scrapy_poet
-from scrapy import Request, Spider
-from web_poet.exceptions import UseFallback
+from scrapy import Spider
 
 from foo.po import SamplePage
 
@@ -193,19 +129,17 @@ class TestSpider(Spider):
     name = "test_spider"
 
     def parse(self, response, page: SamplePage):
-        try:
-            return page.to_item()
-        except UseFallback:
-            return {"foo": "bar"}
+        return page.to_item()
 """
     )
     with MockServer(EchoResource) as server:
-        call_scrapy_command(
-            str(cwd), "savefixture", type_name, server.root_url, "test_spider"
-        )
+        call_scrapy_command(str(cwd), "savefixture", type_name, server.root_url)
 
     fixtures_dir = cwd / "fixtures"
     fixture_dir = fixtures_dir / type_name / "test-1"
     fixture = Fixture(fixture_dir)
     assert fixture.is_valid()
-    assert json.loads(fixture.output_path.read_bytes()) == {"foo": "bar"}
+    assert (
+        json.loads(fixture.exception_path.read_bytes())["type_name"]
+        == "web_poet.exceptions.core.UseFallback"
+    )
