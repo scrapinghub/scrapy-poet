1e8af9a5993196c7f072f57b2e70eabe5d9bc94f
Adrián Chaves <adrian@chaves.io>
2023-11-06 15:21:20 +0100

    Fix circular dependency caused by isort

--- scrapy_poet/__init__.py
+++ scrapy_poet/__init__.py
@@ -1,5 +1,5 @@
-from ._request_fingerprinter import ScrapyPoetRequestFingerprinter
 from .api import DummyResponse, callback_for
 from .downloadermiddlewares import InjectionMiddleware
 from .page_input_providers import HttpResponseProvider, PageObjectInputProvider
 from .spidermiddlewares import RetryMiddleware
+from ._request_fingerprinter import ScrapyPoetRequestFingerprinter
