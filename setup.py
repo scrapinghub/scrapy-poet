5a5d89e96fcda9456d22b9ef8826b81816ef7417
Adrián Chaves <adrian@chaves.io>
2023-11-16 09:49:05 +0100

    Add HttpRequestProvider

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         "time_machine >= 2.2.0",
         "twisted >= 18.9.0",
         "url-matcher >= 0.2.0",
-        "web-poet >= 0.15",
+        "web-poet @ git+https://github.com/Gallaecio/web-poet.git@request-headers-from-bytes",  # https://github.com/scrapinghub/web-poet/pull/191
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
