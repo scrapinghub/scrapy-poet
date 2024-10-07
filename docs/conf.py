3397bf1e3cefc77461b913a7c6cbd84c2186d8ab
Andrey Rakhmatullin <wrar@wrar.name>
2024-10-07 14:08:27 +0500

    Fix docs build with sphinx-rtd-theme 3.0.0.

--- docs/conf.py
+++ docs/conf.py
@@ -99,13 +99,6 @@ pygments_style = None
 #
 html_theme = "sphinx_rtd_theme"
 
-# Add any paths that contain custom themes here, relative to this directory.
-# Add path to the RTD explicitly to robustify builds (otherwise might
-# fail in a clean Debian build env)
-import sphinx_rtd_theme  # noqa: E402
-
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
