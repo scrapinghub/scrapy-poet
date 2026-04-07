5bfcd51cc1257c17ca7979c72be2cdeab74e0dc3
Adrian <adrian@zyte.com>
2026-04-07 11:14:01 +0200

    Add llms.txt and llms-full.txt generation (#237)

--- docs/conf.py
+++ docs/conf.py
@@ -57,11 +57,6 @@ version, release = get_version_and_release()
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.ifconfig",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.githubpages",
     "sphinx_scrapy",
 ]
 
@@ -201,9 +196,7 @@ epub_title = project
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ["search.html"]
 
-
 # -- Extension configuration -------------------------------------------------
-
 
 autodoc_default_options = {
     "special-members": "__init__,__call__",
